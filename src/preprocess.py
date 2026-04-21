8382a9329db44f13a8cc4879f27f1d997a5a1849
NoorhanR <rashidnoorhan@gmail.com>
2026-04-21 13:14:16 -0500

    worked on smote and scaling

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,5 +2,16 @@ from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import SMOTE
 
 # Imbalance Handling using SMOTE
+def apply_smote(X_train, y_train):
+    smote = SMOTE(random_state = 42)
+    X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
+    return X_resampled, y_resampled
 
 # Scale 
+def scale_features(X_train, X_test):
+    scaler = StandardScaler()
+
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    return X_train_scaled, X_test_scaled, scaler
