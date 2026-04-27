9393afe8f6c595200b9d6f46eb5a9c30768993d7
Ezzah-Q <ezzah.aq.094@gmail.com>
2026-04-27 14:24:03 -0500

    worked on main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import sys
  
 # Import each team member's file 
 from load_data   import load_data                  
@@ -16,9 +17,25 @@ NUM_LAYERS = 2         # number of stacked LSTM layers
 LEARNING_RATE = 0.001  # how fast the model updates weights
 EPOCHS = 10            # how many times to loop through training data
 
-# load data
 
-# preprocess
+def main():
+    # make sure user types in 2 commands
+    if len(sys.argv) !=2:
+        print("Usage: python main.py <path_to_dataset>")
+        print("Example: python main/py data/creditcard.csv")
+        sys.exit(1)
+    
+    # get the dataset path from the second command line
+    data_path = sys.argv[1]
+
+    # load data
+    print(f"\n Loading data from: {data_path}")
+
+    # split into training and testing data
+
+
+    # preprocess
+    print()
 
 # create sequences
 
