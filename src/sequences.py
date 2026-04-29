3550354b5d369d1f6665231f58b9af7a163b820b
Prasamsha2 <prasamsha233@gmail.com>
2026-04-29 10:39:30 -0500

    added the LSTM forward pass and created the  sequence

--- src/sequences.py
+++ src/sequences.py
@@ -1,4 +1,15 @@
-#test
-print("Hello, World!")
-#testing this too
-print("This is a test.")
+import numpy as np
+
+def create_sequences(X, y, window_size):
+    sequences = []
+    labels = []
+
+    # make small windows of data so the LSTM can look at past rows
+    for i in range(len(X) - window_size):
+        seq = X[i : i + window_size]
+        label = y[i + window_size]
+
+        sequences.append(seq)
+        labels.append(label)
+
+    return np.array(sequences), np.array(labels)
