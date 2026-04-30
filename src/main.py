c0e66c39377bd337ccb8657e850610e2bcf67e65
Ezzah-Q <ezzah.aq.094@gmail.com>
2026-04-29 20:34:52 -0500

    used sampling, should train model fast now

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import sys
  
-# Import each team member's file 
-#from load_data   import load_data                  
+# Import each team member's file               
 from preprocess  import scale_features, apply_smote  
 from sequences   import create_sequences            
 from model       import LSTMModel                  
@@ -13,14 +11,13 @@ from evaluation  import evaluate_model
 
 # seed for reproducibility (so weight init + SMOTE stay consistent across runs)
 np.random.seed(42)
-     
 
 # hyperparameters (tune these)
-WINDOW_SIZE = 5        # how many transactions the model sees at once
-HIDDEN_SIZE = 64       # size of the hidden state vector
-NUM_LAYERS = 2         # number of stacked LSTM layers
+WINDOW_SIZE = 3        # how many transactions the model sees at once
+HIDDEN_SIZE = 32       # size of the hidden state vector
+NUM_LAYERS = 1         # number of stacked LSTM layers
 LEARNING_RATE = 0.001  # how fast the model updates weights
-EPOCHS = 10            # how many times to loop through training data
+EPOCHS = 3            # how many times to loop through training data
 
 
 def main():
@@ -36,6 +33,12 @@ def main():
     # load features (X) and labels (y) from data
     print(f"\nLoading data from: {data_path}")
     df = pd.read_csv(data_path)
+ 
+    # use stratified sampling to sample randomly from dataset, but keep proportion of fraud to not fraud the same (0.05 equates to about 14,000 rows)
+    fraud = df[df['Class'] == 1].sample(frac=0.05, random_state=42)
+    normal = df[df['Class'] == 0].sample(frac=0.05, random_state=42)
+    df = pd.concat([fraud, normal]).reset_index(drop=True)
+
     # Split into features and label
     X = df.drop(columns=['Class']).values
     y = df['Class'].values
