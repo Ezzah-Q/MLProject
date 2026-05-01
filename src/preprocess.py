418fae2d86227f4cd834480968492d5008ffed80
NoorhanR <rashidnoorhan@gmail.com>
2026-05-01 14:15:40 -0500

    comments and clean up code

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,15 +3,21 @@ from imblearn.over_sampling import SMOTE
 
 # Imbalance Handling using SMOTE
 def apply_smote(X_train, y_train):
+    # Initialize SMOTE to random state
     smote = SMOTE(random_state = 42)
-    X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
-    return X_resampled, y_resampled
+    # Balance
+    X_sample, y_sample = smote.fit_resample(X_train, y_train)
+    # Return 
+    return X_sample, y_sample
 
 # Scale 
 def scale_features(X_train, X_test):
+    # Initialize scaler
     scaler = StandardScaler()
 
+    # Fit training data and transform train and test data
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
 
+    # Return scaled train and tested data
     return X_train_scaled, X_test_scaled, scaler
